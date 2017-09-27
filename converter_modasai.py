c9dbd46e2823fbd57a2fb11dbe81b1d76784a6d7
M. Sonntag <michael.p.sonntag@gmail.com>
2017-09-27 14:32:07 +0200

    [converter] add gaze direction export

--- converter_modasai.py
+++ converter_modasai.py
@@ -201,8 +201,44 @@ def write_tobii_data(b, tobii_data, tobii_offset):
 
     write_tobii_pupil_center(b, group, tobii_data, tobii_offset)
     write_tobii_pupil_diameter(b, group, tobii_data, tobii_offset)
+    write_tobii_gaze_dir(b, group, tobii_data, tobii_offset)
 
     return group
+
+
+def write_tobii_gaze_dir(b, g, tobii_data, tobii_offset):
+    da_left = write_tobii_gaze_dir_eye(b, tobii_data, tobii_offset, "left")
+    g.data_arrays.append(da_left.id)
+
+    da_right = write_tobii_gaze_dir_eye(b, tobii_data, tobii_offset, "right")
+    g.data_arrays.append(da_right.id)
+
+
+def write_tobii_gaze_dir_eye(b, tobii_data, tobii_offset, eye):
+    filtered = filter(lambda y: y["eye"] == eye, filter(lambda x: x.__contains__("gd"), tobii_data))
+    tobii_pc_data = sorted(filtered, key=operator.itemgetter("ts"))
+
+    ts = []
+    combined = []
+    for e in tobii_pc_data:
+        # apply offset to timestamp
+        ts.append(e["ts"] - tobii_offset)
+        coord = e["gd"]
+        combined.append([coord[0], coord[1], coord[2], e["s"]])
+
+    da = b.create_data_array("gaze direction " + eye, "nix.tobii.property", data=combined)
+    da.unit = "mm"
+    da.label = "gaze direction"
+    da.description = "The timestamp has been modified by an offset of -" + str(tobii_offset)
+
+    dim = da.append_range_dimension(ts)
+    dim.unit = "us"
+    dim.label = "timestamp"
+
+    dim = da.append_set_dimension()
+    dim.labels = ["X", "Y", "Z", "error"]
+
+    return da
 
 
 def write_tobii_pupil_center(b, g, tobii_data, tobii_offset):
