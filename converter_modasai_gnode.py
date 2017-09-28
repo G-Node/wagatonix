af3612574a059c333790c19ef3cd2e8fab310748
M. Sonntag <michael.p.sonntag@gmail.com>
2017-09-28 15:31:42 +0200

    [converter] update metadata import message

--- converter_modasai_gnode.py
+++ converter_modasai_gnode.py
@@ -27,7 +27,7 @@ def write_channel_metadata(section, name, gain=100):
 
 
 def write_session_metadata(nixfile, block, metadatafile):
-    print("INFO: Writing metadata from '%s' to NIX" % metadatafile)
+    print("INFO/METADATA: Importing from file '%s'" % metadatafile)
     md_sec = nixfile.create_section(block.name, "recording")
     # rec_sec["experimenter"] = "John Doe"
     # rec_sec["startDate"] = "-".join([block.name[:4], block.name[4:6], block.name[6:8]])
